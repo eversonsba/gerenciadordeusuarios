a5fa974f9c78506bcb65e1ca75643cebc6e9457f
eversonsba <everson.paulino@gmail.com>
2020-03-12 15:56:24 -0300

    Criação do projeto

---
+++ src/test/java/com/gerenciadordeusuarios/GerenciadordeusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciadordeusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadordeusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
