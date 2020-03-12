d41f96436c300d4e32c749f8aacec4cc11ae857c
eversonsba <everson.paulino@gmail.com>
2020-03-12 18:50:02 -0300

    Service Layer

--- src/main/java/com/gerenciadordeusuarios/resources/CargosResource.java
+++ src/main/java/com/gerenciadordeusuarios/resources/CargosResource.java
@@ -1,20 +1,35 @@
 package com.gerenciadordeusuarios.resources;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gerenciadordeusuarios.entidades.Cargos;
+import com.gerenciadordeusuarios.services.CargosService;
 
 @RestController
 @RequestMapping(value = "/cargos")
 public class CargosResource {
 	
+	@Autowired
+	private CargosService service;
+	
 	@GetMapping
-	public ResponseEntity<Cargos> findAll(){
-		Cargos c = new Cargos("Programador");
-		return ResponseEntity.ok().body(c);
+	public ResponseEntity<List<Cargos>> findAll(){
+		List<Cargos> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	@GetMapping(value = "/{nome}")
+	public ResponseEntity<Cargos> findById (@PathVariable String nome){
+		Cargos obj = service.findById(nome);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
---
+++ src/main/java/com/gerenciadordeusuarios/services/CargosService.java
@@ -0,0 +1,25 @@
+package com.gerenciadordeusuarios.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gerenciadordeusuarios.entidades.Cargos;
+import com.gerenciadordeusuarios.repositories.CargosRepository;
+
+@Service
+public class CargosService {
+	
+	@Autowired
+	private CargosRepository repository;
+	
+	public List<Cargos> findAll(){
+		return repository.findAll();
+	}
+	public Cargos findById(String nome) {
+		Optional <Cargos> obj = repository.findById(nome);
+		return obj.get();
+	}
+}
